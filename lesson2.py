693bdddc30d33fb4fdb6da74b0b57a72593d3920
D1moha <m.dimoha@gmail.com>
2021-12-13 20:09:36 +0200

    add lesson2.py

--- lesson2.py
+++ lesson2.py
@@ -9,29 +9,29 @@ print ("Исходный список:", listOriginal)
 print ("- - -")
 
 #1 - сортируем список по возрастанию
-listSortByAscending = sorted(listOriginal, key = lambda x: x[1])
-print ("1. Список по возрастанию:", listSortByAscending)
+listSortByAscending = sorted(listOriginal, key = lambda x: x[1]) #сортируем список по второму элементу (по возрастанию)
+print ("1. Список по возрастанию:", listSortByAscending) #выводим список
 print ("- - -")
 
 #2 - создаём словарь
-dicNew = dict(zip([x.pop(1) for x in listSortByAscending], [x for x in listSortByAscending]))
-print ("2. Словарь:", dicNew)
+dicNew = dict(zip([x.pop(1) for x in listSortByAscending], [x for x in listSortByAscending])) #создаём словарь, где ключ - это второй элемент списка, а значения - остальыне элементы 
+print ("2. Словарь:", dicNew) #выводим словарь
 print ("- - -")
     
 #3 - сортируем значения в словаре по убыванию
-dicValues = list(dicNew.values())
+dicValues = list(dicNew.values()) #выбираем значения из словаря в виде списка
 for x in dicValues:
-    x.sort(reverse = True)
-dicSortValuesByDescending = dict(zip(dicNew.keys(),dicValues))
-print ("3. Словарь со значениями по убыванию:", dicSortValuesByDescending)
+    x.sort(reverse = True) #сортируем значения по убыванию
+dicSortValuesByDescending = dict(zip(dicNew.keys(),dicValues)) #формируем словарь с отсортированными значения
+print ("3. Словарь со значениями по убыванию:", dicSortValuesByDescending) #выводим словарь
 print ("- - -")
 
 #4 - получаем множество из всех значений словаря
-setDicValues = dicSortValuesByDescending.values()
-setNew = set([item for sublist in setDicValues for item in sublist])
-print("4. Множество из значений словаря:", setNew)
+setDicValues = dicSortValuesByDescending.values() #выбираем значения из словаря
+setNew = set([item for sublist in setDicValues for item in sublist]) #формируем множество
+print("4. Множество из значений словаря:", setNew) #выводим множество
 print ("- - -")
 
 #5 - преобразуем множество в строку
-strNew = ''.join(map(str,setNew))
+strNew = ''.join(map(str,setNew)) #формируем строку из элементов множества, меняя тип каждого элемента, без разделителя
 print ("5. Строка:", strNew)
